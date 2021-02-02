387ea60bc050deb3a36f37d55af1d4b689590183
isajediknight <luke@isajediknight.com>
2021-02-02 00:59:14 -0600

    [Metrics] Adding logic for additional exchange order book calls

--- classes/crypto_metrics.py
+++ classes/crypto_metrics.py
@@ -91,10 +91,72 @@ class Metrics:
         self.crypto_main = main
         self.crypto_alt = alt
 
+        self.crypto_pair = CryptoPairs('tradeogre', main, alt)
+
     def call_order_book(self,exchange='tradeogre'):
 
         if(exchange in list(self.exchange_keys.keys())):
-            if
+            if exchange == 'tradeogre':
+                self.crypto_pair.set_exchange('tradeogre')
+                api_tradeogre = TradeOgre(self.exchange_keys['tradeogre'].get_key(), self.exchange_keys['tradeogre'].get_secret())
+                tradeogre_json_result = api_tradeogre.order_book(self.crypto_pair.get_corrected_pair_for_exchange())
+                #tradeogre_buy = tradeogre_json_result['buy']
+                #tradeogre_sell = tradeogre_json_result['sell']
+                return tradeogre_json_result['buy'],tradeogre_json_result['sell']
+            elif exchange == 'binance':
+                exchange_id = exchange
+                self.crypto_pair.set_exchange(exchange_id)
+                exchange_class = getattr(ccxt, exchange_id)
+                exchange = exchange_class({
+                    'apiKey': exchange_keys[exchange_id].get_key(),
+                    'secret': exchange_keys[exchange_id].get_secret(),
+                    'timeout': 30000,
+                    'enableRateLimit': True,
+                })
+                exchange.verbose = False
+                # dict keys ['bids', 'asks', 'timestamp', 'datetime', 'nonce']
+                binance_dict_result = exchange.fetch_order_book(crypto_pair.get_corrected_pair_for_exchange())  # 'BTC/USDT')
+                #binance_buy = binance_dict_result['bids']
+                #binance_sell = binance_dict_result['asks']
+                return binance_dict_result['bids'], binance_dict_result['asks']
+            elif exhange == 'bittrex':
+                exchange_id = exhange
+                crypto_pair.set_exchange(exchange_id)
+                exchange_class = getattr(ccxt, exchange_id)
+                exchange = exchange_class({
+                    #    'apiKey': exchange_keys[exchange_id].get_key(),
+                    #    'secret': exchange_keys[exchange_id].get_secret(),
+                    'timeout': 30000,
+                    'enableRateLimit': True,
+                })
+                exchange.verbose = False
+                # dict keys ['bids', 'asks', 'timestamp', 'datetime', 'nonce']
+                bittrex_dict_result = exchange.fetch_order_book(
+                    crypto_pair.get_corrected_pair_for_exchange())  # 'BTC/USDT')
+                # print(list(depth.keys()))
+                #bittrex_buy = bittrex_dict_result['bids']
+                #bittrex_sell = bittrex_dict_result['asks']
+                return bittrex_dict_result['bids'], bittrex_dict_result['asks']
+            elif exchange == 'kraken':
+                exchange_id = 'kraken'
+                crypto_pair.set_exchange(exchange_id)
+                exchange_class = getattr(ccxt, exchange_id)
+                exchange = exchange_class({
+                    #    'apiKey': exchange_keys[exchange_id].get_key(),
+                    #    'secret': exchange_keys[exchange_id].get_secret(),
+                    'timeout': 30000,
+                    'enableRateLimit': True,
+                })
+                exchange.verbose = False
+                # dict keys ['bids', 'asks', 'timestamp', 'datetime', 'nonce']
+                kraken_dict_result = exchange.fetch_order_book(
+                    crypto_pair.get_corrected_pair_for_exchange())  # 'BTC/USDT')
+                # print(list(depth.keys()))
+                #kraken_buy = kraken_dict_result['bids']
+                #kraken_sell = kraken_dict_result['asks']
+                return kraken_dict_result['bids'],kraken_dict_result['asks']
+            else:
+                print("Exchange " + exhcange + " has not been properly added to ../classes/crypto_metrics.py")
         else:
             raise Exception(" Exchange " + exchange + " information is not present in the keys file: " + INPUTS_DIR + 'untitled.pyc')
 
